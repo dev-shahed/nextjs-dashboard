c886b235ea5715bae18c4d95a7747fde0b8abfdb
dev-shahed <shahedthedev@gmail.com>
2024-12-25 23:52:45 +0600

    done: customer page, dynamic meta data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Metadata } from "next";
+import React, { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
+import { InvoiceSkeleton } from "@/app/ui/skeletons";
 
-export default function Page() {
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = 3;
+
   return (
-    <div>Customers Page</div>
-  )
+    <>
+      <Suspense fallback={<InvoiceSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+    </>
+  );
 }
